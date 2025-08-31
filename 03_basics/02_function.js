3fece05d2280d7fba34d8052bc5aaa0aa421a2e3
Devansh@125 <cdevansh913@gmail.com>
2025-08-31 20:13:12 +0530

    Basic Function

---
+++ 03_basics/02_function.js
@@ -0,0 +1,5 @@
+function calculateCartPrice(...num1){ // here ... act as a rest operator we see that this is used as a spread oprator also
+  return num1
+}
+
+console.log(calculateCartPrice(100 ,200 , 300)) // this returns an array 
